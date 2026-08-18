4ca92300e024c8d6272eb45565065612d3fb8386
Rakesh <reddyavrakesh@gmail.com>
2026-08-18 21:41:56 +0530

    Day-7 of internship - JavaScript

---
+++ Day_7_17AUG2026/index.js
@@ -0,0 +1,29 @@
+let Name = "Rakesh";
+let rollNo = "249X5A02C2";
+
+console.log(Name);
+console.log(rollNo);
+
+console.log();
+
+console.log("My Name is " + Name);
+console.log("My Roll No is " + rollNo);
+
+console.log();
+
+console.log(typeof 32);
+console.log(typeof "Name");
+console.log(typeof true);
+console.log(typeof null);
+
+const ar = [1, 3, 4, 5, 6];
+
+console.log(typeof ar);
+
+var age = 20;
+
+if (age >= 18) {
+  console.log("You are Eligible for Vote");
+} else {
+  console.log("Your are not Eligible for vote");
+}
